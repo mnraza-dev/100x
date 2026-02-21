68aa2bcd8ebb29be18e3404e95fe9bb8d78c6cd5
MN Raza <mnraza.dev@gmail.com>
2026-02-21 16:50:06 +0530

    add delete button and add todo button functionality using DOM

--- HTML-DOM/script.js
+++ HTML-DOM/script.js
@@ -1,1 +1,19 @@
-console.log('hi there');
+function addTodo() {
+    const el = document.getElementById('todosInput');
+    const todo = el.value;
+
+    const todoDiv = document.createElement('div');
+    const todoSpan = document.createElement('span');
+    todoSpan.innerHTML = todo;
+    todoDiv.setAttribute('class', 'todoItem');
+    todoDiv.appendChild(todoSpan);
+
+    const delButton = document.createElement('button');
+    delButton.innerHTML = 'Delete Todo';
+    delButton.addEventListener("click", function () {
+        this.parentElement.remove();
+    });
+    todoDiv.appendChild(delButton);
+    document.getElementById('todos').appendChild(todoDiv);
+
+}
